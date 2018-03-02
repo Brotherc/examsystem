26d328778251cf8b67ee79efece2843ad29ace3c
Brotherc <1157730239@qq.com>
2018-03-02 11:53:41 +0800

    学生Vo

---
+++ examsystem-rest/examsystem-rest-pojo/src/main/java/cn/examsystem/rest/pojo/vo/StudentVo.java
@@ -0,0 +1,18 @@
+package cn.examsystem.rest.pojo.vo;
+
+import cn.examsystem.rest.pojo.po.Student;
+
+/**
+ * Created by Administrator on 2018/2/4.
+ * 学生视图类
+ */
+public class StudentVo extends Student{
+    @Override
+    public void setName(String name) {
+        try {
+            System.out.println("初始------"+name);
+            super.setName(new String(name.getBytes("iso8859-1"), "utf-8"));
+        }catch (Exception e){
+        }
+    }
+}
