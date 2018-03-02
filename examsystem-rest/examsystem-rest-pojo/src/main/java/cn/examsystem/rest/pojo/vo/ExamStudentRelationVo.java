bc9f2387749780f2ef25b5584f231afd5c14aee3
Brotherc <1157730239@qq.com>
2018-03-02 11:53:28 +0800

    考试学生关系Vo

---
+++ examsystem-rest/examsystem-rest-pojo/src/main/java/cn/examsystem/rest/pojo/vo/ExamStudentRelationVo.java
@@ -0,0 +1,18 @@
+package cn.examsystem.rest.pojo.vo;
+
+import cn.examsystem.rest.pojo.dto.ExamStudentRelationDto;
+
+/**
+ * Created by Administrator on 2018/2/6.
+ */
+public class ExamStudentRelationVo extends ExamStudentRelationDto{
+
+    @Override
+    public void setStudentName(String studentName) {
+        try {
+            System.out.println("初始------"+studentName);
+            super.setStudentName(new String(studentName.getBytes("iso8859-1"), "utf-8"));
+        }catch (Exception e){
+        }
+    }
+}
