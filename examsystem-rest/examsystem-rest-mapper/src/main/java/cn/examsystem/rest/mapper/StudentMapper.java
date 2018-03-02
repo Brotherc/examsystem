340926f473b1257b2b9e748f742b002e5844c990
Brotherc <1157730239@qq.com>
2018-03-02 11:58:24 +0800

    学生mapper

---
+++ examsystem-rest/examsystem-rest-mapper/src/main/java/cn/examsystem/rest/mapper/StudentMapper.java
@@ -0,0 +1,31 @@
+package cn.examsystem.rest.mapper;
+
+import cn.examsystem.rest.pojo.po.Student;
+import cn.examsystem.rest.pojo.po.StudentExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface StudentMapper {
+    int countByExample(StudentExample example);
+
+    int deleteByExample(StudentExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(Student record);
+
+    int insertSelective(Student record);
+
+    List<Student> selectByExample(StudentExample example);
+
+    Student selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") Student record, @Param("example") StudentExample example);
+
+    int updateByExample(@Param("record") Student record, @Param("example") StudentExample example);
+
+    int updateByPrimaryKeySelective(Student record);
+
+    int updateByPrimaryKey(Student record);
+}
