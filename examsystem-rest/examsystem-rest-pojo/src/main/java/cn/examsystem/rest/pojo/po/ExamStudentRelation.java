47e19170cf52a9a1924986716bddda2ccbb251aa
Brotherc <1157730239@qq.com>
2018-03-02 11:52:17 +0800

    考试学生关系实体

---
+++ examsystem-rest/examsystem-rest-pojo/src/main/java/cn/examsystem/rest/pojo/po/ExamStudentRelation.java
@@ -0,0 +1,96 @@
+package cn.examsystem.rest.pojo.po;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class ExamStudentRelation {
+    private String id;
+
+    private String examId;
+
+    private String studentId;
+
+    private Integer status;
+
+    private BigDecimal score;
+
+    private Boolean isGraded;
+
+    private Integer partOrder;
+
+    private Date createdTime;
+
+    private Date updatedTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getExamId() {
+        return examId;
+    }
+
+    public void setExamId(String examId) {
+        this.examId = examId == null ? null : examId.trim();
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId == null ? null : studentId.trim();
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public BigDecimal getScore() {
+        return score;
+    }
+
+    public void setScore(BigDecimal score) {
+        this.score = score;
+    }
+
+    public Boolean getIsGraded() {
+        return isGraded;
+    }
+
+    public void setIsGraded(Boolean isGraded) {
+        this.isGraded = isGraded;
+    }
+
+    public Integer getPartOrder() {
+        return partOrder;
+    }
+
+    public void setPartOrder(Integer partOrder) {
+        this.partOrder = partOrder;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public Date getUpdatedTime() {
+        return updatedTime;
+    }
+
+    public void setUpdatedTime(Date updatedTime) {
+        this.updatedTime = updatedTime;
+    }
+}
