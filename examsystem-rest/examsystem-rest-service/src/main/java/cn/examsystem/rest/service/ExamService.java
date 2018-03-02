81551ad4223511bffe6a4c1454a063191c8bf199
Brotherc <1157730239@qq.com>
2018-03-02 12:00:40 +0800

    添加功能接口 （ 为考试添加学生， 为考试添加学生（Excel导入）， 根据条件查询考试学生， 将学生从考试中移除， 修改考试学生场次， 查询某门考试未添加的正常状态的学生， 启动考试， 根据学生id查询正在进行的考试， 考试 ）

--- examsystem-rest/examsystem-rest-service/src/main/java/cn/examsystem/rest/service/ExamService.java
+++ examsystem-rest/examsystem-rest-service/src/main/java/cn/examsystem/rest/service/ExamService.java
@@ -2,7 +2,12 @@ package cn.examsystem.rest.service;
 
 import cn.examsystem.common.pojo.ResultInfo;
 import cn.examsystem.rest.pojo.dto.ExamDto;
+import cn.examsystem.rest.pojo.dto.ExamStudentRelationDto;
+import cn.examsystem.rest.pojo.dto.StudentDto;
 import cn.examsystem.rest.pojo.po.Exam;
+import cn.examsystem.rest.pojo.po.Student;
+import cn.examsystem.rest.pojo.vo.ExamStudentRelationVo;
+import cn.examsystem.rest.pojo.vo.StudentVo;
 
 import java.util.List;
 
@@ -20,4 +25,38 @@ public interface ExamService {
 
     //更新考试
     public ResultInfo updateExam(String id, Exam exam) throws Exception;
+
+    //为考试添加学生
+    public ResultInfo addStudentForExam(String id, StudentDto studentDto) throws Exception;
+
+    public ResultInfo addStudentsForExam(String id, String[] studentIds) throws Exception;
+
+    //为考试添加学生（Excel导入）
+    public ResultInfo addStudentForExamByExcel(String id,String fileName, byte[] uploadData) throws Exception;
+
+
+
+    //根据条件查询考试学生
+    public List<ExamStudentRelationDto>listExamStudent(ExamStudentRelationVo examStudentRelationVo) throws Exception;
+
+    //将学生从考试中移除
+    public ResultInfo removeStudentFromExam(String examStudentRelationId) throws Exception;
+
+    //修改考试学生场次
+    public ResultInfo updateExamStudentPartOrder(String examStudentRelationId,Integer partOrdeer) throws Exception;
+
+    //查询某门考试未添加的正常状态的学生
+    public List<Student> listStudentNoExistExam(String examId,StudentVo studentVo) throws Exception;
+
+    //启动考试
+    public ResultInfo startExam(String id)throws Exception;
+
+
+
+    //根据学生id查询正在进行的考试
+    public ExamStudentRelationDto getProceedExamByLoginStudentId(String studentId) throws Exception;
+
+    //考试
+    public ResultInfo test(ExamStudentRelationDto examStudentRelationDto) throws Exception;
+
 }
