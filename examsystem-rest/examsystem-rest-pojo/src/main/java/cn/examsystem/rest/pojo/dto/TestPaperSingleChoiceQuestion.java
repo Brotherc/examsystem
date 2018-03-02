c62b7fc31e1eceaf6e5e19e00cd80d533db59dab
Brotherc <1157730239@qq.com>
2018-03-02 11:51:22 +0800

    试卷单选题Dto

---
+++ examsystem-rest/examsystem-rest-pojo/src/main/java/cn/examsystem/rest/pojo/dto/TestPaperSingleChoiceQuestion.java
@@ -0,0 +1,51 @@
+package cn.examsystem.rest.pojo.dto;
+
+import cn.examsystem.rest.pojo.po.TestpaperQuestionRelation;
+
+public class TestPaperSingleChoiceQuestion extends TestpaperQuestionRelation{
+    private String questionContent;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+    private String optionD;
+
+    public String getQuestionContent() {
+        return questionContent;
+    }
+
+    public void setQuestionContent(String questionContent) {
+        this.questionContent = questionContent;
+    }
+
+    public String getOptionA() {
+        return optionA;
+    }
+
+    public void setOptionA(String optionA) {
+        this.optionA = optionA;
+    }
+
+    public String getOptionB() {
+        return optionB;
+    }
+
+    public void setOptionB(String optionB) {
+        this.optionB = optionB;
+    }
+
+    public String getOptionC() {
+        return optionC;
+    }
+
+    public void setOptionC(String optionC) {
+        this.optionC = optionC;
+    }
+
+    public String getOptionD() {
+        return optionD;
+    }
+
+    public void setOptionD(String optionD) {
+        this.optionD = optionD;
+    }
+}
