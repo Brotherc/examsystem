5f81e56d2ed72ed2ef4d36a7aa45fdc3ab8125c1
Brotherc <1157730239@qq.com>
2018-03-02 13:52:03 +0800

    教师端考试控制器添加请求 （ 为考试添加学生， 为考试添加学生（Excel导入）， 根据条件查询考试学生， 将学生从考试中移除， 修改考试学生场次， 修改学生信息， 查询某门考试未添加的正常状态的学生， 启动考试 ）

--- examsystem-manager-web/src/main/java/cn/examsystem/manager/controller/ExamController.java
+++ examsystem-manager-web/src/main/java/cn/examsystem/manager/controller/ExamController.java
@@ -2,13 +2,19 @@ package cn.examsystem.manager.controller;
 
 import cn.examsystem.common.pojo.ResultInfo;
 import cn.examsystem.manager.utils.RestTemplateUtils;
+import cn.examsystem.rest.pojo.dto.StudentDto;
 import cn.examsystem.rest.pojo.po.Exam;
+import cn.examsystem.rest.pojo.po.ExamStudentRelation;
+import cn.examsystem.rest.pojo.po.Student;
+import cn.examsystem.rest.pojo.vo.ExamStudentRelationVo;
+import cn.examsystem.rest.pojo.vo.StudentVo;
 import cn.examsystem.security.pojo.dto.SysuserDto;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
 
@@ -25,6 +31,16 @@ public class ExamController {
     private String REST_BASE_URL;
     @Value("${EXAM_URL}")
     private String EXAM_URL;
+    @Value("${EXAM_STUDENT_URL}")
+    private String EXAM_STUDENT_URL;
+    @Value("${EXAM_FILE_URL}")
+    private String EXAM_FILE_URL;
+    @Value("${EXAMSTUDENT_URL}")
+    private String EXAMSTUDENT_URL;
+    @Value("${EXAMSTUDENT_EXAM_URL}")
+    private String EXAMSTUDENT_EXAM_URL;
+    @Value("${EXAM_STATUS_URL}")
+    private String EXAM_STATUS_URL;
 
     @Value("${MESSAGE_GET_FAIL}")
     private String MESSAGE_GET_FAIL;
@@ -105,4 +121,151 @@ public class ExamController {
         }
         return resultInfo;
     }
+
+    @PostMapping("/v1/exam/{id}/student")
+    public ResultInfo addStudentForExam(@PathVariable String id,StudentDto studentDto) throws Exception{
+
+        ResultInfo resultInfo;
+        try {
+            //调用rest服务
+            resultInfo=RestTemplateUtils.exchange(REST_BASE_URL+EXAM_URL+"/{id}"+EXAM_STUDENT_URL,HttpMethod.POST,studentDto,ResultInfo.class,new Object[]{id});
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResultInfo(ResultInfo.STATUS_RESULT_INTERANL_SERVER_ERROR,MESSAGE_SAVE_FAIL,null);
+        }
+        return resultInfo;
+    }
+
+    @PostMapping("/v1/exam/{id}/students")
+    public ResultInfo addStudentsForExam(@PathVariable String id,@RequestParam(value = "studentIds[]") String[] studentIds) throws Exception{
+
+        ResultInfo resultInfo;
+        try {
+            //调用rest服务
+            resultInfo=RestTemplateUtils.exchange(REST_BASE_URL+EXAM_URL+"/{id}"+EXAM_STUDENT_URL+"s",HttpMethod.POST,studentIds,ResultInfo.class,new Object[]{id});
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResultInfo(ResultInfo.STATUS_RESULT_INTERANL_SERVER_ERROR,MESSAGE_SAVE_FAIL,null);
+        }
+        return resultInfo;
+    }
+
+    @PostMapping("/v1/exam/{id}/student/file")
+    public ResultInfo addStudentForExamByExcel(@PathVariable String id, MultipartFile upload) throws Exception{
+
+        System.out.println(upload.getOriginalFilename());
+
+        ResultInfo resultInfo;
+        try {
+            //调用rest服务
+            resultInfo=RestTemplateUtils.exchange(REST_BASE_URL+EXAM_URL+"/{id}"+EXAM_STUDENT_URL+EXAM_FILE_URL+"?fileName="+upload.getOriginalFilename(),HttpMethod.POST,upload.getBytes(),ResultInfo.class,new Object[]{id});
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResultInfo(ResultInfo.STATUS_RESULT_INTERANL_SERVER_ERROR,MESSAGE_SAVE_FAIL,null);
+        }
+        return resultInfo;
+    }
+
+
+    @GetMapping("/v1/exam/{id}/student")
+    public ResultInfo listExamStudent(@PathVariable String id, ExamStudentRelationVo examStudentRelationVo) throws Exception{
+
+        ResultInfo resultInfo;
+        try{
+            //将查询参数构建在url后面
+            JSONObject obj=new JSONObject(examStudentRelationVo);
+            String url = expandURL(REST_BASE_URL + EXAM_URL+"/{id}"+EXAM_STUDENT_URL+"?", obj);
+
+            System.out.print(url);
+
+            //调用rest服务
+            resultInfo = RestTemplateUtils.exchange(url, HttpMethod.GET, ResultInfo.class,new Object[]{id});
+            System.out.println("---------"+resultInfo);
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("---------"+"失败");
+            return new ResultInfo(ResultInfo.STATUS_RESULT_INTERANL_SERVER_ERROR,MESSAGE_GET_FAIL,null);
+        }
+        return resultInfo;
+    }
+
+    @DeleteMapping("/v1/exam/student/{examStudentRelationId}")
+    public ResultInfo removeStudentFromExam(@PathVariable String examStudentRelationId) throws Exception{
+
+        ResultInfo resultInfo;
+        try {
+            //调用rest服务
+            resultInfo=RestTemplateUtils.exchange(REST_BASE_URL+EXAM_URL+EXAM_STUDENT_URL+"/{examStudentRelationId}",HttpMethod.DELETE,ResultInfo.class,new Object[]{examStudentRelationId});
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResultInfo(ResultInfo.STATUS_RESULT_INTERANL_SERVER_ERROR,MESSAGE_DELETE_FAIL,null);
+        }
+        return resultInfo;
+    }
+
+    @PutMapping("/v1/exam/student/{examStudentRelationId}")
+    public ResultInfo updateExamStudentPartOrder(@PathVariable String examStudentRelationId, ExamStudentRelation examStudentRelation) throws Exception{
+
+        ResultInfo resultInfo;
+        try {
+            //调用rest服务
+            resultInfo=RestTemplateUtils.exchange(REST_BASE_URL+EXAM_URL+EXAM_STUDENT_URL+"/{examStudentRelationId}",HttpMethod.PUT,examStudentRelation,ResultInfo.class,new Object[]{examStudentRelationId});
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResultInfo(ResultInfo.STATUS_RESULT_INTERANL_SERVER_ERROR,MESSAGE_UPDATE_FAIL,null);
+        }
+        return resultInfo;
+    }
+
+    @PutMapping("/v1/examStudent/{id}")
+    public ResultInfo updateStudent(@PathVariable String id, Student student) throws Exception{
+
+        ResultInfo resultInfo;
+        try {
+            //调用rest服务
+            resultInfo=RestTemplateUtils.exchange(REST_BASE_URL+EXAMSTUDENT_URL+"/{id}",HttpMethod.PUT,student,ResultInfo.class,new Object[]{id});
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResultInfo(ResultInfo.STATUS_RESULT_INTERANL_SERVER_ERROR,MESSAGE_UPDATE_FAIL,null);
+        }
+        return resultInfo;
+    }
+
+
+    @GetMapping("/v1/examStudent/exam/{id}")
+    public ResultInfo listStudentNoExistExam(@PathVariable String id, StudentVo studentVo) throws Exception{
+
+        ResultInfo resultInfo;
+        try{
+            //将查询参数构建在url后面
+            JSONObject obj=new JSONObject(studentVo);
+            String url = expandURL(REST_BASE_URL+EXAMSTUDENT_URL+EXAMSTUDENT_EXAM_URL+"/{id}"+"?", obj);
+
+            System.out.print(url);
+
+            //调用rest服务
+            resultInfo = RestTemplateUtils.exchange(url, HttpMethod.GET, ResultInfo.class,new Object[]{id});
+            System.out.println("---------"+resultInfo);
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("---------"+"失败");
+            return new ResultInfo(ResultInfo.STATUS_RESULT_INTERANL_SERVER_ERROR,MESSAGE_GET_FAIL,null);
+        }
+        return resultInfo;
+    }
+
+    @PutMapping("/v1/exam/{id}/status")
+    public ResultInfo startExam(@PathVariable String id) throws Exception{
+
+        ResultInfo resultInfo;
+        try {
+            //调用rest服务
+            resultInfo=RestTemplateUtils.exchange(REST_BASE_URL+EXAM_URL+"/{id}"+EXAM_STATUS_URL,HttpMethod.PUT,ResultInfo.class,new Object[]{id});
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResultInfo(ResultInfo.STATUS_RESULT_INTERANL_SERVER_ERROR,MESSAGE_UPDATE_FAIL,null);
+        }
+        return resultInfo;
+    }
+
 }
