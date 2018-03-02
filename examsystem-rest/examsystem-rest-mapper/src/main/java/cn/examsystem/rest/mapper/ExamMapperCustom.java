27fe6d66072f8adc44f6897f203f7030ebbf04c8
Brotherc <1157730239@qq.com>
2018-03-02 11:56:07 +0800

    添加查询考试学生关系接口

--- examsystem-rest/examsystem-rest-mapper/src/main/java/cn/examsystem/rest/mapper/ExamMapperCustom.java
+++ examsystem-rest/examsystem-rest-mapper/src/main/java/cn/examsystem/rest/mapper/ExamMapperCustom.java
@@ -1,10 +1,14 @@
 package cn.examsystem.rest.mapper;
 
 import cn.examsystem.rest.pojo.dto.ExamDto;
+import cn.examsystem.rest.pojo.dto.ExamStudentRelationDto;
 import cn.examsystem.rest.pojo.po.Exam;
+import cn.examsystem.rest.pojo.vo.ExamStudentRelationVo;
 
 import java.util.List;
 
 public interface ExamMapperCustom {
     List<ExamDto> listExam(Exam exam);
+
+    List<ExamStudentRelationDto> listExamStudent(ExamStudentRelationVo examStudentRelationVo);
 }
