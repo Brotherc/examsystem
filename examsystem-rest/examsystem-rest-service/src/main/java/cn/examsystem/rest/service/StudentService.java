630d6e36e1b3846a206109c88747b20fe32ae3ab
Brotherc <1157730239@qq.com>
2018-03-07 20:56:09 +0800

    添加查询学生，添加学生的功能接口

--- examsystem-rest/examsystem-rest-service/src/main/java/cn/examsystem/rest/service/StudentService.java
+++ examsystem-rest/examsystem-rest-service/src/main/java/cn/examsystem/rest/service/StudentService.java
@@ -2,6 +2,9 @@ package cn.examsystem.rest.service;
 
 import cn.examsystem.common.pojo.ResultInfo;
 import cn.examsystem.rest.pojo.po.Student;
+import cn.examsystem.rest.pojo.vo.StudentVo;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2018/1/30.
@@ -11,4 +14,11 @@ public interface StudentService {
 
     //更新学生
     public ResultInfo updateStudent(String id, Student student) throws Exception;
+
+    //根据条件查询学生
+    public List<Student> listStudent(StudentVo studentVo) throws Exception;
+
+    //添加学生
+    public ResultInfo saveStudent(Student student) throws Exception;
+
 }
