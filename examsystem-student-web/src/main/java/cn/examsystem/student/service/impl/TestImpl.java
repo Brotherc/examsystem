b069c32b816eddd0fa21026613e7b09eb34ef20e
Brotherc <1157730239@qq.com>
2018-03-20 13:51:13 +0800

    业务改进

--- examsystem-student-web/src/main/java/cn/examsystem/student/service/impl/TestImpl.java
+++ examsystem-student-web/src/main/java/cn/examsystem/student/service/impl/TestImpl.java
@@ -41,30 +41,29 @@ public class TestImpl implements TestService {
 
         //单选题数量不能为空
         Integer singleChoiceQuestionNum = testPaperDto.getSingleChoiceQuestionNum();
-        if(singleChoiceQuestionNum==null)
-            return new ResultInfo(ResultInfo.STATUS_RESULT_UNPROCESABLE_ENTITY,MESSAGE_QUESTION_NUM_NOT_NULL,null);
+        if(singleChoiceQuestionNum!=null){
+            //学生试卷单选题答题信息
+            Map<Integer, String> singleChoiceQuestionAnswer = testPaperDto.getSingleChoiceQuestionAnswer();
 
-        //学生试卷单选题答题信息
-        Map<Integer, String> singleChoiceQuestionAnswer = testPaperDto.getSingleChoiceQuestionAnswer();
+            Map< Integer, String> redisMap=new LinkedHashMap<Integer, String>();
+            //如果用户没写就提交，设置答案全为空
 
-        Map< Integer, String> redisMap=new LinkedHashMap<Integer, String>();
-        //如果用户没写就提交，设置答案全为空
+            //如果用户全写了，则中间为null的答案设置为“”
 
-        //如果用户全写了，则中间为null的答案设置为“”
-
-        if(singleChoiceQuestionAnswer==null||singleChoiceQuestionAnswer.size()<1){
-            for(int i=1;i<=singleChoiceQuestionNum;i++){
-                redisMap.put(i, "");
+            if(singleChoiceQuestionAnswer==null||singleChoiceQuestionAnswer.size()<1){
+                for(int i=1;i<=singleChoiceQuestionNum;i++){
+                    redisMap.put(i, "");
+                }
+            }else{
+                for(int i=1;i<=singleChoiceQuestionNum;i++){
+                    String answer = singleChoiceQuestionAnswer.get(i);
+                    if(StringUtils.isBlank(answer))
+                        answer="";
+                    redisMap.put(i, answer);
+                }
             }
-        }else{
-            for(int i=1;i<=singleChoiceQuestionNum;i++){
-                String answer = singleChoiceQuestionAnswer.get(i);
-                if(StringUtils.isBlank(answer))
-                    answer="";
-                redisMap.put(i, answer);
-            }
+            jedisClient.hset(examStudentId, REDIS_KEY_SINGLE_CHOICE_QUESTION_ANSWER, JsonUtils.objectToJson(redisMap));
         }
-        jedisClient.hset(examStudentId, REDIS_KEY_SINGLE_CHOICE_QUESTION_ANSWER, JsonUtils.objectToJson(redisMap));
 
         return new ResultInfo(ResultInfo.STATUS_RESULT_CREATED,MESSAGE_SAVE_SUCCESS,null);
     }
@@ -74,31 +73,30 @@ public class TestImpl implements TestService {
 
         //判断题数量不能为空
         Integer trueOrFalseQuestionNum = testPaperDto.getTrueOrFalseQuestionNum();
-        if(trueOrFalseQuestionNum==null)
-            return new ResultInfo(ResultInfo.STATUS_RESULT_UNPROCESABLE_ENTITY,MESSAGE_QUESTION_NUM_NOT_NULL,null);
-
-        //学生试卷判断题答题信息
-        Map<Integer, String> trueOrFalseQuestionAnswer = testPaperDto.getTrueOrFalseQuestionAnswer();
+        if(trueOrFalseQuestionNum!=null){
+            //学生试卷判断题答题信息
+            Map<Integer, String> trueOrFalseQuestionAnswer = testPaperDto.getTrueOrFalseQuestionAnswer();
 
 
-        Map< Integer, String> redisMap=new LinkedHashMap<Integer, String>();
-        //如果用户没写就提交，设置答案全为空
+            Map< Integer, String> redisMap=new LinkedHashMap<Integer, String>();
+            //如果用户没写就提交，设置答案全为空
 
-        //如果用户全写了，则中间为null的答案设置为“”
+            //如果用户全写了，则中间为null的答案设置为“”
 
-        if(trueOrFalseQuestionAnswer==null||trueOrFalseQuestionAnswer.size()<1){
-            for(int i=1;i<=trueOrFalseQuestionNum;i++){
-                redisMap.put(i, "");
+            if(trueOrFalseQuestionAnswer==null||trueOrFalseQuestionAnswer.size()<1){
+                for(int i=1;i<=trueOrFalseQuestionNum;i++){
+                    redisMap.put(i, "");
+                }
+            }else{
+                for(int i=1;i<=trueOrFalseQuestionNum;i++){
+                    String answer = trueOrFalseQuestionAnswer.get(i);
+                    if(StringUtils.isBlank(answer))
+                        answer="";
+                    redisMap.put(i, answer);
+                }
             }
-        }else{
-            for(int i=1;i<=trueOrFalseQuestionNum;i++){
-                String answer = trueOrFalseQuestionAnswer.get(i);
-                if(StringUtils.isBlank(answer))
-                    answer="";
-                redisMap.put(i, answer);
-            }
+            jedisClient.hset(examStudentId, REDIS_KEY_TRUE_OR_FALSE_QUESTION_ANSWER, JsonUtils.objectToJson(redisMap));
         }
-        jedisClient.hset(examStudentId, REDIS_KEY_TRUE_OR_FALSE_QUESTION_ANSWER, JsonUtils.objectToJson(redisMap));
 
         return new ResultInfo(ResultInfo.STATUS_RESULT_CREATED,MESSAGE_SAVE_SUCCESS,null);
     }
@@ -108,19 +106,18 @@ public class TestImpl implements TestService {
 
         //填空题数量不能为空
         Integer fillInBlankQuestionNum = testPaperDto.getFillInBlankQuestionNum();
-        if(fillInBlankQuestionNum==null)
-            return new ResultInfo(ResultInfo.STATUS_RESULT_UNPROCESABLE_ENTITY,MESSAGE_QUESTION_NUM_NOT_NULL,null);
+        if(fillInBlankQuestionNum!=null){
+            //学生试卷填空题答题信息
+            Map<Integer, List> fillInBlankQuestionAnswer = testPaperDto.getFillInBlankQuestionAnswer();
 
-        //学生试卷填空题答题信息
-        Map<Integer, List> fillInBlankQuestionAnswer = testPaperDto.getFillInBlankQuestionAnswer();
+            Map< Integer, List> redisMap=new LinkedHashMap<Integer, List>();
 
-        Map< Integer, List> redisMap=new LinkedHashMap<Integer, List>();
-
-        for(int i=1;i<=fillInBlankQuestionAnswer.size();i++){
-            List<String> list = fillInBlankQuestionAnswer.get(i);
-            redisMap.put(i, list);
+            for(int i=1;i<=fillInBlankQuestionAnswer.size();i++){
+                List<String> list = fillInBlankQuestionAnswer.get(i);
+                redisMap.put(i, list);
+            }
+            jedisClient.hset(examStudentId, REDIS_KEY_FILL_IN_BLANK_QUESTION_ANSWER, JsonUtils.objectToJson(redisMap));
         }
-        jedisClient.hset(examStudentId, REDIS_KEY_FILL_IN_BLANK_QUESTION_ANSWER, JsonUtils.objectToJson(redisMap));
 
         return new ResultInfo(ResultInfo.STATUS_RESULT_CREATED,MESSAGE_SAVE_SUCCESS,null);
     }
